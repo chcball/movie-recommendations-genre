983616d6735274a5c66cccb1a59462d4561dbf56
Yllennor Anne Carbonell <99461301+chcball@users.noreply.github.com>
2024-05-26 22:58:48 +0800

    Update movie-recommendations-genre.py

--- movie-recommendations-genre.py
+++ movie-recommendations-genre.py
@@ -1,12 +1,13 @@
 import streamlit as st
+import openai
 import random
 
 # Dictionary of movies categorized by genre
 movies = {
-    "action": ["The Dark Knight", "Inception", "Mad Max: Fury Road", "Die Hard"],
-    "comedy": ["The Hangover", "Superbad", "Dumb and Dumber", "Anchorman"],
-    "drama": ["The Shawshank Redemption", "The Godfather", "Forrest Gump", "Schindler's List"],
-    "horror": ["The Shining", "Get Out", "A Quiet Place", "The Conjuring"]
+    "action": ["Atlas", "The Fall Guy", "Mad Max: Fury Road", "Die Hard"],
+    "comedy": ["Legally Blonde", "White Chicks", "Matilda", "Family Switch"],
+    "drama": ["Little Women", "20th Century Girl", "Purple Hearts", "Five Feet Apart"],
+    "horror": ["The Medium", "Gonjiam:Haunted Asylum", "A Quiet Place", "The Conjuring"]
 }
 
 # Function to recommend a movie based on genre
@@ -18,8 +19,8 @@ def recommend_movie(genre):
 
 # Main function to run the Streamlit app
 def main():
-    st.title("Movie Recommender")
-    st.subheader("Welcome to Movie Recommender! Get personalized movie recommendations based on your preferred genre.")
+    st.title("CineMatch")
+    st.subheader("Welcome to CineMatch:Your Personal Movie Companion! Get personalized movie recommendations based on your preferred genre.")
 
     # Ask user for their preferred genre
     genre = st.selectbox("Select your preferred movie genre:", ["Action", "Comedy", "Drama", "Horror"])
